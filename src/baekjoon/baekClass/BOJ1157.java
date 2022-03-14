45e2fe643eccc2dea09f6affa2573a0f193b0f19
dong yeol <ehfql6363@naver.com>
2022-03-14 21:04:46 +0900

    Changes

--- src/baekjoon/baekClass/BOJ1157.java
+++ src/baekjoon/baekClass/BOJ1157.java
@@ -7,8 +7,27 @@ public class BOJ1157 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        int[] alpha = new int[26];
+        int max = Integer.MIN_VALUE;
+        char ans = '?';
 
-        
+        String a = br.readLine();
+        String str = a.toUpperCase();
+
+        for(int i=0; i<a.length(); i++){
+            alpha[str.charAt(i) - 'A']++;
+        }
+
+        for(int i=0; i<alpha.length; i++){
+            if(alpha[i] > max) ans = (char)(i + 65);
+            else if(alpha[i] == max) ans = '?';
+            System.out.print(alpha[i]+" ");
+        }
+        System.out.println();
+
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }

e38e931e175b9b7de13db498146f60994a34bc84
dong yeol <ehfql6363@naver.com>
2022-04-18 21:27:20 +0900

    Changes

--- src/baekjoon/BOJ11021.java
+++ src/baekjoon/BOJ11021.java
@@ -1,28 +1,12 @@
 package baekjoon;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class BOJ11021 {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int t = Integer.parseInt(br.readLine());
-        int cnt = 0;
-        while(t>0){
-            t--;
-            cnt++;
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            sb.append("Case #").append(cnt).append(": ").append(a+b).append("\n");
-        }
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+
     }
 }
---
+++ src/baekjoon/BOJ4673.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class BOJ4673 {
+    public static void main(String[] args) {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[10001];
+
+
+    }
+    public static void brute(int num){
+        int a = num
+    }
+}

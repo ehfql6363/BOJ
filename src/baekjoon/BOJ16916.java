7d77b3e272291b5793aa3a83337bed5829d7cec0
dong yeol <ehfql6363@naver.com>
2022-07-20 23:46:28 +0900

    Done

--- src/baekjoon/BOJ16916.java
+++ src/baekjoon/BOJ16916.java
@@ -6,52 +6,13 @@ public class BOJ16916 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String text = sc.next();
-        String pattern = sc.next();
+        String str1 = sc.next();
+        String str2 = sc.next();
 
-        System.out.println(find(text, pattern));
-    }
-    static int find(String parent, String pattern) {
-        int[] table = makeTable(pattern);
-        int n1 = parent.length();
-        int n2 = pattern.length();
-
-        int idx = 0;
-        for(int i=0; i< n1; i++) {
-            while(idx>0 && parent.charAt(i) != pattern.charAt(idx)) {
-                idx = table[idx-1];
-            }
-
-            if(parent.charAt(i) == pattern.charAt(idx)) {
-                if(idx == n2-1) {
-                    idx = table[idx];
-                    return 1;
-                }else {
-                    idx += 1;
-                }
-            }
+        if(str1.contains(str2)) {
+            System.out.println(1);
+        }else{
+            System.out.println(0);
         }
-
-        return 0;
-
-    }
-
-    static int[] makeTable(String pattern) {
-        int n = pattern.length();
-        int[] table = new int[n];
-
-        int idx=0;
-        for(int i=1; i<n; i++) {
-            while(idx>0 && pattern.charAt(i) != pattern.charAt(idx)) {
-                idx = table[idx-1];
-            }
-
-            if(pattern.charAt(i) == pattern.charAt(idx)) {
-                idx += 1;
-                table[i] = idx;
-            }
-        }
-
-        return table;
     }
 }

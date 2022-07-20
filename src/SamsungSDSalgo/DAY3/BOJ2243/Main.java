12371b7b3822439f8d4aef9dd797b3b0d560b5e4
김동열 <ehfql6363@naver.com>
2022-07-21 02:13:15 +0900

    20220720 삼성 SDS 알고리즘 특강 DAY3

--- src/SamsungSDSalgo/DAY3/BOJ2243/Main.java
+++ src/SamsungSDSalgo/DAY3/BOJ2243/Main.java
@@ -5,9 +5,8 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n ,S, index;
-    static long[] arr = new long[1000001];
-    static long[] tree;
+    static int n ,S = 1000001;
+    static int[] tree;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,30 +16,29 @@ public class Main {
 
         n = Integer.parseInt(br.readLine());
 
-        S = 1;
-        while (S < 1000000) {
-            S *= 2;
-        }
-
-        tree = new long[2 * S];
+        /**
+         * S = 1;
+         * while (S < n) {
+         *     S *= 2;
+         * }
+         *
+         * 위 방법으로 안되는 이유
+         */
+        tree = new int[4 * S];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
 
             if (a == 1) { // 사탕 빼기
-                index = 0;
                 int b = Integer.parseInt(st.nextToken()); // b번째 사탕
-                pop(1, S, 1, b);
-                sb.append(index).append("\n");
+                sb.append(pop(1, S, 1, b)).append("\n");
             } else if (a == 2) { // 사탕 넣기
-                int b = Integer.parseInt(st.nextToken());
-                long c = Long.parseLong(st.nextToken());
+                int b = Integer.parseInt(st.nextToken()); // 사탕 맛
+                int c = Integer.parseInt(st.nextToken()); // 해당 사탕의 개수
+                update(1, S, 1, b, c);
 
-                arr[b] += c;
             }
-
-            init();
         }
 
         bw.write(sb.toString());
@@ -48,39 +46,33 @@ public class Main {
         bw.close();
         br.close();
     }
-
-    static void init() {
-
-        // Leaf에 값 반영
-        for (int i = 0; i < arr.length - 1; i++) {
-            tree[i + S] = arr[i + 1];
-        }
-        // tree나머지 값 채우기
-        for (int i = S-1; i > 0; i--) {
-            tree[i] = tree[i * 2] + tree[i * 2 + 1];
-        }
-
-        System.out.println(tree[1]);
-        System.out.println("tree[2] = " + tree[2]);
-        System.out.println("tree[3] = " + tree[3]);
-
-    }
-
-    static void pop(int left, int right, int node, int target) {
+    static int pop(int left, int right, int node, int target) {
         int mid = (left + right) / 2;
+
         if (left == right){ //리프 노드에 도달하면
-            tree[node]--;
-            index = node - S + 1;
-            return;
+            update(1, S, 1, left, -1);
+            return left;
         }
-        if (tree[node * 2] <= target) {
-//            System.out.println("Left");
-            pop(left, mid, node * 2, target);
+
+        if (tree[node * 2] >= target) {
+            return pop(left, mid, node * 2, target);
         }else{
-//            System.out.println("Right");
             target -= tree[node * 2];
-            pop(mid + 1, right, node * 2 + 1, target);
+            return pop(mid + 1, right, node * 2 + 1, target);
         }
     }
 
+    static void update(int left, int right, int node, int target, int diff) {
+        // 범위를 벗어난 경우 ( 무의미 )
+        if (target < left || target > right) return;
+        else{
+            tree[node] += diff;
+
+            if (left != right){
+                int mid = (left + right) / 2;
+                update(left, mid, node * 2, target, diff); //왼쪽 자식
+                update(mid + 1, right, node * 2 + 1, target, diff); //오른쪽 자식
+            }
+        }
+    }
 }
